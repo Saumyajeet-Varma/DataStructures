e72154b1f61e93eab83413226d6fed2020d69363
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-06-19 22:33:17 +0530

    cpp/graph/adjacencymatrix

---
+++ datastructures-cpp/Graph/adjacencyMatrix.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class undirectedAdjacencyMatrix
+{
+    vector<vector<int>> unweightedAdjacencyMatrix(int v, int e, vector<vector<int>> edges)
+    {
+        vector<vector<int>> adj(v + 1, vector<int>(v + 1, 0));
+
+        for (vector<int> edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u][v] = 1;
+            adj[v][u] = 1;
+        }
+
+        return adj;
+    }
+
+    vector<vector<int>> weightedAdjacencyMatrix(int v, int e, vector<vector<int>> edges)
+    {
+        vector<vector<int>> adj(v + 1, vector<int>(v + 1, 0));
+
+        for (vector<int> edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            adj[u][v] = w;
+            adj[v][u] = w;
+        }
+
+        return adj;
+    }
+};
+
+class directedAdjacencyMatrix
+{
+    vector<vector<int>> unweightedAdjacencyMatrix(int v, int e, vector<vector<int>> edges)
+    {
+        vector<vector<int>> adj(v + 1, vector<int>(v + 1, 0));
+
+        for (vector<int> edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u][v] = 1;
+        }
+
+        return adj;
+    }
+
+    vector<vector<int>> weightedAdjacencyMatrix(int v, int e, vector<vector<int>> edges)
+    {
+        vector<vector<int>> adj(v + 1, vector<int>(v + 1, 0));
+
+        for (vector<int> edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            adj[u][v] = w;
+        }
+
+        return adj;
+    }
+};
